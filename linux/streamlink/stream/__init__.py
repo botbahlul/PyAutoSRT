50663cf4bb1bee61ca32f580e01d73815bc414ff
Bot Bahlul <88623122+botbahlul@users.noreply.github.com>
2026-08-30 22:05:57 +0800

    Delete linux/streamlink directory

--- linux/streamlink/stream/__init__.py
+++
@@ -1,6 +0,0 @@
-from streamlink.stream.dash import DASHStream
-from streamlink.stream.ffmpegmux import MuxedStream
-from streamlink.stream.hls import HLSStream, MuxedHLSStream
-from streamlink.stream.http import HTTPStream
-from streamlink.stream.stream import Stream, StreamIO
-from streamlink.stream.wrappers import StreamIOIterWrapper, StreamIOThreadWrapper, StreamIOWrapper
